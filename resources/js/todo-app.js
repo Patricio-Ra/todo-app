17875c519a2752ad74e0836d9702ab820505e99c
Patricio-Ra <patricio.raschetti@gmail.com>
2019-10-07 23:29:23 -0300

    Filter feature

--- resources/js/todo-app.js
+++ resources/js/todo-app.js
@@ -20,24 +20,59 @@ const todos = [{
   completed: false
 }];
 
-const incompletedTodos = todos.filter(todo => {
-  return !todo.completed;
-});
-const summary = document.createElement('h2');
-summary.textContent = `You have ${incompletedTodos.length} todos left to complete.`;
-document.querySelector('body').appendChild(summary);
+const filters = {
+  searchText: ''
+}
+
+
+// Add Todo input.
+document.querySelector('#new-todo').addEventListener('input', (e) => console.log(e.target.value));
+
+document.querySelector('#add-todo').addEventListener('click', (e) => console.log('TODO added!'));
+
+
+// Filter Todos Input.
+document.querySelector('#filter-todos').addEventListener('input', (e) => {
+  filters.searchText = e.target.value;
+  renderTodos(todos, filters);
+})
+
+
+// Render Todos & Todos left.
+const renderTodos = function (todos, filters) {
+  // Todos.
+  const filteredTodos = todos.filter(todo => {
+    return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+  });
+
+  // Clean the container.
+  document.querySelector('#todos').innerHTML = '';
+
+  // Todos left.
+  const incompletedTodos = filteredTodos.filter(todo => {
+    return !todo.completed;
+  });
+
+  // Render Todos left.
+  const summary = document.createElement('h2');
+  summary.textContent = `You have ${incompletedTodos.length} todos left.`;
+  document.querySelector('#todos').appendChild(summary);
+
+  // Render Todos.
+  filteredTodos.forEach(todo => {
+    let todoElement = document.createElement('p');
+    todoElement.textContent = `${todo.text}: ${todo.completed ? 'Completed' : 'Incompleted'}`;
+    document.querySelector('#todos').appendChild(todoElement);
+  });
+}
+
+renderTodos(todos, filters);
+
 
 // let incompletedTodosNum = 0;
 // const incompletedTodos = todos.forEach(todo => {
 //   !todo.completed ? incompletedTodosNum += 1 : incompletedTodosNum += 0;
 // });
-
-todos.forEach(todo => {
-  let todoP = document.createElement('p');
-  todoP.textContent = todo.text;
-  document.querySelector('body').appendChild(todoP);
-});
-
 
 
 
