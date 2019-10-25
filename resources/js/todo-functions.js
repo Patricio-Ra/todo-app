e0960efb59f02596060cc3e6708845c800c7dea5
Patricio-Ra <patricio.raschetti@gmail.com>
2019-10-25 17:08:58 -0300

    Add try/catch to the localStorage possible error

--- resources/js/todo-functions.js
+++ resources/js/todo-functions.js
@@ -3,7 +3,11 @@
 // Fetch existing data from localStorage
 const getSavedTodos = () => {
   const todosJSON = localStorage.getItem('todos');
-  return todosJSON ? JSON.parse(todosJSON) : [];
+  try {
+    return todosJSON ? JSON.parse(todosJSON) : [];
+  } catch (e) {
+    return [];
+  };
 };
 
 
