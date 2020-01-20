784a19ecdf205830a00aba18c93a08330190963e
Patricio R <patricio.raschetti@gmail.com>
2020-01-20 20:24:14 -0300

    Removed Bootstrap

--- resources/js/todo-app.js
+++ resources/js/todo-app.js
@@ -3,35 +3,36 @@
 const todos = getSavedTodos();
 
 const filters = {
-  'searchText': '',
-  'hideCompleted': false
+	'searchText': '',
+	'hideCompleted': false
 };
 
 renderTodos(todos, filters);
 
 document.querySelector('#filter-todos').addEventListener('input', e => {
-  filters.searchText = e.target.value;
-  renderTodos(todos, filters);
+	filters.searchText = e.target.value;
+	renderTodos(todos, filters);
 });
 
 document.querySelector('#hide-completed').addEventListener('change', e => {
-  filters.hideCompleted = e.target.checked;
-  renderTodos(todos, filters);
+	filters.hideCompleted = e.target.checked;
+	renderTodos(todos, filters);
 });
 
 document.querySelector('#new-todo-form').addEventListener('submit', e => {
-  e.preventDefault();
-  if (e.target.elements.newTodoInput.value.length) {
-    const newTodo = {
-      id: uuidv4(),
-      text: e.target.elements.newTodoInput.value,
-      completed: false
-    };
-    todos.push(newTodo);
-    e.target.elements.newTodoInput.value = '';
-    saveTodos(todos)
-  } else {
-    return false;
-  };
-  renderTodos(todos, filters);
+	e.preventDefault();
+	if (e.target.elements.newTodoInput.value.length) {
+		const newTodo = {
+			id: uuidv4(),
+			text: e.target.elements.newTodoInput.value,
+			completed: false
+		};
+		todos.push(newTodo);
+		e.target.elements.newTodoInput.value = '';
+		saveTodos(todos)
+	} else {
+		return false;
+	};
+
+	renderTodos(todos, filters);
 });
--- resources/js/todo-functions.js
+++ resources/js/todo-functions.js
@@ -2,102 +2,99 @@
 
 // Fetch existing data from localStorage
 const getSavedTodos = () => {
-  const todosJSON = localStorage.getItem('todos');
-  try {
-    return todosJSON ? JSON.parse(todosJSON) : [];
-  } catch (e) {
-    return [];
-  };
+	const todosJSON = localStorage.getItem('todos');
+	try {
+		return todosJSON ? JSON.parse(todosJSON) : [];
+	} catch (e) {
+		return [];
+	};
 };
 
 
 // Save data to localStorage
 const saveTodos = todos => {
-  localStorage.setItem('todos', JSON.stringify(todos));
+	localStorage.setItem('todos', JSON.stringify(todos));
 };
 
 
 // Remove todo
 const removeTodo = id => {
-  const todoIndex = todos.findIndex(todo => todo.id === id);
+	const todoIndex = todos.findIndex(todo => todo.id === id);
 
-  if (todoIndex > -1) {
-    todos.splice(todoIndex, 1);
-  };
+	if (todoIndex > -1) {
+		todos.splice(todoIndex, 1);
+	};
 };
 
 
 // Toggle completed status with checkboxes.
 const setCompleted = id => {
-  const todo = todos.find(todo => todo.id === id);
+	const todo = todos.find(todo => todo.id === id);
 
-  if (todo) {
-    todo.completed = !todo.completed;
-  };
+	if (todo) {
+		todo.completed = !todo.completed;
+	};
 };
 
 
 // Generate DOM elements for an individual note.
 const generateTodo = todo => {
-  const todoElement = document.createElement('div');
-  const todoCheck = document.createElement('input');
-  const todoText = document.createElement('span');
-  const deleteBtn = document.createElement('button');
-
-  todoElement.className = 'h5 text-center my-3 py-2 border rounded border-light';
+	const todoElement = document.createElement('div');
+	const todoCheck = document.createElement('input');
+	const todoText = document.createElement('span');
+	const deleteBtn = document.createElement('button');
 
   // Setup todo checkbox.
-  todoCheck.setAttribute('type', 'checkbox');
-  todoCheck.checked = todo.completed;
-  todoElement.appendChild(todoCheck);
-  todoCheck.addEventListener('change', e => {
-    setCompleted(todo.id);  // Instead it's better to todo.completed = e.target.checked;*
-    saveTodos(todos);
-    renderTodos(todos, filters);
-  });
+	todoCheck.setAttribute('type', 'checkbox');
+	todoCheck.checked = todo.completed;
+	todoElement.appendChild(todoCheck);
+	todoCheck.addEventListener('change', e => {
+		setCompleted(todo.id);  // Instead it's better to todo.completed = e.target.checked;*
+		saveTodos(todos);
+		renderTodos(todos, filters);
+	});
 
   // Setup todo text.
-  todoText.textContent = todo.text;
-  todoElement.appendChild(todoText);
-  
-  // Setup delete button.
-  deleteBtn.textContent = 'x';
-  todoElement.appendChild(deleteBtn);
-  deleteBtn.addEventListener('click', e => {
-    removeTodo(todo.id);
-    saveTodos(todos);
-    renderTodos(todos, filters);
-  });
+	todoText.textContent = todo.text;
+	todoElement.appendChild(todoText);
 
-  return todoElement;
+  // Setup delete button.
+	deleteBtn.textContent = 'x';
+	todoElement.appendChild(deleteBtn);
+	deleteBtn.addEventListener('click', e => {
+		removeTodo(todo.id);
+		saveTodos(todos);
+		renderTodos(todos, filters);
+	});
+
+	return todoElement;
 };
 
 
 // Generate DOM elements for the summary.
 const generateSummary = incompletedTodos => {
 
-  const summary = document.createElement('h2');
-  summary.textContent = `You have ${incompletedTodos.length} todos left.`;
+	const summary = document.createElement('h2');
+	summary.textContent = `You have ${incompletedTodos.length} todos left.`;
 
-  return summary;
+	return summary;
 };
 
 
 // Render app based on filters.
 const renderTodos = (todos, filters) => {
-  const filteredTodos = todos.filter(todo => {
-    const textFiltering = todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
-    const completedFiltering = !filters.hideCompleted || !todo.completed;
+	const filteredTodos = todos.filter(todo => {
+		const textFiltering = todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+		const completedFiltering = !filters.hideCompleted || !todo.completed;
+		return textFiltering && completedFiltering;
+		});
 
-    return textFiltering && completedFiltering;
-    });
+	const incompletedTodos = filteredTodos.filter(todo => !todo.completed);
 
-  const incompletedTodos = filteredTodos.filter(todo => !todo.completed);
-
-  document.querySelector('#todos').innerHTML = '';
-  document.querySelector('#todos').appendChild(generateSummary(incompletedTodos));
-  filteredTodos.forEach(todo => {
-    document.querySelector('#todos').appendChild(generateTodo(todo));
-  });
+	document.querySelector('#todos').innerHTML = '';
+	document.querySelector('#todos').appendChild(generateSummary(incompletedTodos));
+	filteredTodos.forEach(todo => {
+		document.querySelector('#todos').appendChild(generateTodo(todo));
+	});
 };
 
